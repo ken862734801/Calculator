d0bb9618fae4873804144b9b51a57aa61da2ffc2
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-06-25 02:54:09 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,43 @@
 
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator-keys');
+const display = document.querySelector(".calculator-display");
 
+keys.addEventListener("click", e=>{
+    if(e.target.matches("button")){
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        if(!action){
+            console.log("number key!")
+            if(displayedNum === "0"){
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+        if(
+            action === "addition" ||
+            action === "subtraction" ||
+            action === "multiplication" ||
+            action === "division"
+        ){
+            console.log("operator key!")
+        }
+        if(action === "decimal"){
+            display.textContent = displayedNum + ".";
+            console.log("decimal key!")
+        }
+        if(action === "clear"){
+            display.textContent = "";
+            console.log("clear key!")
+        }
+        if(action === "enter"){
+            console.log("equals key!")
+        }
+    }
+})
 
   function colorChanger(color){
       switch(color){
