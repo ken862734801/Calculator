3ad01485f030147cbb5d75f43c636bfc047b3098
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2024-01-17 22:05:15 -0500

    add basic calculate on enter btn click

--- src/app/components/Calculator.tsx
+++ src/app/components/Calculator.tsx
@@ -36,6 +36,10 @@ const list = [
         type: 'function',
         value: ['clear'],
     },
+    {
+        type: 'function',
+        value: ['enter'],
+    },
 ];
 
 interface CalculatorProps {
@@ -45,12 +49,10 @@ interface CalculatorProps {
 function Calculator({ theme }: CalculatorProps) {
     const [isOn, setIsOn] = useState(true);
     const [isAlt, setIsAlt] = useState(false);
-
-    let curr: string[] = [];
+    const [inputValue, setInputValue] = useState<string[]>(['0']);
 
     function clear() {
-        curr = ['0'];
-        console.log(curr);
+        setInputValue(['0']);
     }
 
     function turnOnCalculator() {
@@ -62,6 +64,16 @@ function Calculator({ theme }: CalculatorProps) {
     function turnOffCalculator() {
         console.log('Turning off...');
         setIsOn(false);
+    }
+
+    function calculate() {
+        const input = inputValue.join('');
+        try {
+            const response = useMath.evaluate(input);
+            setInputValue([response]);
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     function handleButtonClick(e: any) {
@@ -80,11 +92,18 @@ function Calculator({ theme }: CalculatorProps) {
                 button.dataset.type === 'number' ||
                 button.dataset.type === 'operator'
             ) {
-                curr.push(button.value);
-                console.log(curr);
+                setInputValue((prev) => {
+                    if (prev[0] === '0') {
+                        return [button.value];
+                    } else {
+                        return [...prev, button.value];
+                    }
+                });
             } else if (button.dataset.type === 'function') {
                 if (button.value === 'clear') {
                     clear();
+                } else if (button.value === 'enter') {
+                    calculate();
                 }
             }
             return;
@@ -95,6 +114,15 @@ function Calculator({ theme }: CalculatorProps) {
         <div>
             <h5>TI-30XS</h5>
             <h6>MultiView</h6>
+            <div className="display">
+                {isOn && (
+                    <>
+                        {inputValue.map((value, index) => (
+                            <span key={index}>{value}</span>
+                        ))}
+                    </>
+                )}
+            </div>
             {list.map((item) => (
                 <Button
                     key={`${item.value}-key`}
