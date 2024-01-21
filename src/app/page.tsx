f9dd3374419be18ecec48ec7dca9c5dbf93d955d
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2024-01-21 11:21:30 -0500

    update theme button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,21 @@ export default function Home() {
                 setTheme('pink');
                 break;
         }
-    };
+    }
 
     return (
         <main>
             <Calculator theme={theme} />
             <div>
-                <button onClick={() => toggleTheme('blue')}>BLUE</button>
-                <button onClick={() => toggleTheme('pink')}>PINK</button>
+                <button
+                    className="w-[30px] h-[30px] border border-solid border-black rounded-full bg-[var(--blue)] hover:bg-[var(--blue-hover)]"
+                    onClick={() => toggleTheme('blue')}
+                ></button>
+                <button
+                    className="w-[30px] h-[30px] border border-solid border-black rounded-full bg-[var(--pink)] hover:bg-[var(--pink-hover)]"
+                    onClick={() => toggleTheme('pink')}
+                ></button>
             </div>
         </main>
     );
-};
+}
